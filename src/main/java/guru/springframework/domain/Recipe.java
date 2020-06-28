a509ecfbf485f1a2d8ecefaedd4eeee5feca5981
brandonthecraig <BrandonCraig@rentalcars.com>
2020-06-28 13:31:25 +0100

    Added notes and recipe and created a OneToOne relationship

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,30 @@
+package guru.springframework.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+}
